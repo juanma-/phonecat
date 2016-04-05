b84eebf774fb0b6c09625080878cdf6ab7be3e32
Juan Manuel Castillo Mendoza <jcastillo@ayesa.com>
2016-04-05 17:49:37 +0200

    Organize imports

--- backend/repositories/src/main/java/org/juanma/phonecat/repositories/RepositoriesContext.java
+++ backend/repositories/src/main/java/org/juanma/phonecat/repositories/RepositoriesContext.java
@@ -1,18 +1,18 @@
 package org.juanma.phonecat.repositories;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.Properties;
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 
 /**
